14589cbdc9056c71a2b81352745a27bd4be2e9ec
Rulio <21203943@etu.unicaen.fr>
2014-02-20 21:59:03 +0100

    Finalisation de la page inscription avec envoi de mail pour confirmations

--- header.php
+++ header.php
@@ -1,40 +1,52 @@
-<!DOCTYPE html>
-<html>
 <head>
-    <title></title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-    <script src="http://www.locavenue.fr/js/bootstrap.js"></script>
-
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <script src="http://205.236.12.51/projet/h2014/equipe3/js/bootstrap.js"></script>
 </head>
-<body>
 
 
-<nav style="background-color: #6ad43e " class="navbar navbar-inverse" role="navigation">
-    <div>
+<nav style="z-index: 9999;position: fixed;width: 100%;" class="navbar navbar-inverse" role="navigation">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <div class="navbar-brand" href="#">MemoShot</div>
+        </div>
 
-
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-            <a class="navbar-brand" href="../view/accueilView.php">Memoshot </a>
-
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
+                <li class="active"><a href="#"> <span class="glyphicon glyphicon-home"></span>&nbspHome</a></li>
+            </ul>
+            <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-info"><span class="glyphicon glyphicon-search"></span>&nbspResearch
+                </button>
+            </form>
+
+
+            <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Parametre <b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span
+                            class="glyphicon glyphicon-cog"></span>&nbspSettings <b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li><a href="#">Modifier le compte</a></li>
-                        <li><a href="#">S'abonner </a></li>
+                        <li><a href="#">Modification Compte</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">Abonement</a></li>
                     </ul>
                 </li>
             </ul>
-
-            <input type="button" class="btn btn-danger" value=" Se Deconnecter " onclick="location.href='../'">
         </div>
-
-
+        <!-- /.navbar-collapse -->
     </div>
+    <!-- /.container-fluid -->
 </nav>
-
-
-</body>
-</html>
