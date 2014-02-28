a9af1ce9129b1fe6af53b120c870f9e6875a3567
Mathieu Molinengo <mmolinengo2@gmail.com>
2014-02-28 08:22:54 -0500

    Correction orthographique et des lignes

--- Config/ConnexionBD.php
+++ Config/ConnexionBD.php
@@ -1,17 +1,10 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: Julien
- * Date: 07/02/14
- * Time: 10:34
- */
-class ConnexionsBD
+class ConnexionBD
 {
-
     var $bdd;
 
-    function connexions()
+    function connexion()
     {
         $ini_array = parse_ini_file("../Config/login_db.ini");
         $user = $ini_array["user"];
@@ -22,13 +15,10 @@ class ConnexionsBD
         $bdd = mysql_connect($host, $user, $pass) or die('Impossible de se connecter: ' . mysql_error());
         // Connection à la base de donnée
         mysql_select_db($base, $bdd) or die('Impossible de se connecter: ' . mysql_error());
-
     }
 
-    function deconections()
+    function deconnexion()
     {
         mysql_close($this->bdd);
     }
-
-
 }
