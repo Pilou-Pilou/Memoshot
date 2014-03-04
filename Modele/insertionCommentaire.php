17388fcc6d2e26c8937a9b34bf8d6b151a1a555b
Rulio <21203943@etu.unicaen.fr>
2014-03-04 17:15:21 +0100

    Correction l'ajout de commentaire

--- Modele/insertionCommentaire.php
+++ Modele/insertionCommentaire.php
@@ -7,5 +7,5 @@ $publication = $_POST["publication"];
 
 $connexions = new ConnexionBD();
 $connexions->connexion();
-mysql_query('INSERT INTO commentaires(id_publication,id_auteur,commentaire) VALUES (' . $publication . ',' . $_SESSION['id'] . ',\'' . $commentaire . '\')')
+mysql_query('INSERT INTO commentaires(id_publication,id_auteur,commentaire) VALUES (' . $publication . ',' . $_SESSION['id'] . ', "' . $commentaire . '" )')
 or die (mysql_error() . "Impossible de se connecter à la table commentaires");
