f49984f13c6f1ad994d969775191ad412493eef5
Rulio <21203943@etu.unicaen.fr>
2014-03-07 14:16:27 +0100

    cryptage des données dans l'url

--- Config/stripe-php-1.11.0/montant.php
+++ Config/stripe-php-1.11.0/montant.php
@@ -1,8 +1,2 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mathieu
- * Date: 14-02-18
- * Time: 11:06
- */
 echo '<FORM action=' . "'bouton.php'" . 'method=' . "'post'" . '>' . 'montant <input id="montant" name="montant" rows=1 cols=10 />' . '<INPUT type="submit" name="valider" value="Valider">' . '</FORM>';
